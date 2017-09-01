e1850838d8a3215f3902b536292a0ceb9dbd1ca7
geniushuangxiao <geniushuangxiao@163.com>
2017-09-01 23:04:46 +0800

    增加后端代码，修改前端版本号

---
+++ oblogs-server/src/main/java/com/oblogs/OblogsServerApplication.java
@@ -0,0 +1,12 @@
+package com.oblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OblogsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OblogsServerApplication.class, args);
+	}
+}
