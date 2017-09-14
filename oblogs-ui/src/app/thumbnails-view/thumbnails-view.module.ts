a079c401711ce2706e997a11412adc11df11e7c0
geniushuangxiao <geniushuangxiao@163.com>
2017-09-14 08:21:56 +0800

    缩略图item组件创建

---
+++ oblogs-ui/src/app/thumbnails-view/thumbnails-view.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemViewComponent } from './item-view/item-view.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ItemViewComponent]
+})
+export class ThumbnailsViewModule { }
